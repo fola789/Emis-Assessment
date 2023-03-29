f14f8fc3d88153f8acd9a3de925541b842ea5e1d
fola789 <49055102+fola789@users.noreply.github.com>
2023-03-29 16:08:28 +0100

    Create SQLQuery2 ~ Find Patients per Area Code with Gender Ratio.sql

---
+++ SQLQuery2 ~ Find Patients per Area Code with Gender Ratio.sql
@@ -0,0 +1,14 @@
+SELECT 
+    LEFT(["postcode"], PATINDEX('%[0-9]%', ["postcode"])-1) AS postcode_area, 
+    COUNT(*) AS num_patients,
+    SUM(CASE WHEN ["gender"] = 'Male' THEN 1 ELSE 0 END) AS male_patients,
+    SUM(CASE WHEN ["gender"] = 'Female' THEN 1 ELSE 0 END) AS female_patients,
+    CAST(SUM(CASE WHEN ["gender"] = 'Male' THEN 1 ELSE 0 END) AS FLOAT) / NULLIF(SUM(CASE WHEN ["gender"] = 'Female' THEN 1 ELSE 0 END), 0) AS gender_ratio
+FROM 
+    patient
+WHERE 
+    ["postcode"] LIKE '[A-Z]%[0-9]%'
+GROUP BY 
+    LEFT(["postcode"], PATINDEX('%[0-9]%', ["postcode"])-1)
+ORDER BY 
+    num_patients DESC;
