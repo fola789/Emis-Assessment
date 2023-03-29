282d0d7cd99e00339c4c3af90de3f2cc3e0acae3
fola789 <49055102+fola789@users.noreply.github.com>
2023-03-29 17:46:41 +0100

    Update SQLQuery3 ~ Bulk insert medication 2 to 15 to existing medication table.sql

--- SQLQuery3 ~ Bulk insert medication 2 to 15 to existing medication table.sql
+++ SQLQuery3 ~ Bulk insert medication 2 to 15 to existing medication table.sql
@@ -8,11 +8,10 @@ DECLARE @filename VARCHAR(100)
 WHILE @i <= 15 -- Set the ending number of the CSV file
 BEGIN
     SET @filename = 'C:\Users\Fola\Documents\Projects\EMIS\exa-data-analyst-assessment-main\data\medication\medication_' + CAST(@i AS VARCHAR(2)) + '.csv'
-    DECLARE @sql NVARCHAR(MAX) = N'BULK INSERT #temp_medication FROM ''' + @filename + N''' WITH (FIELDTERMINATOR = '','', ROWTERMINATOR = ''0x0a'')'
-    EXEC(@sql)
-    SET @i = @i + 1
+	EXEC('BULK INSERT #temp_medication FROM ''' + @filename + ''' WITH (FIELDTERMINATOR = '','', ROWTERMINATOR = ''0x0a'')')
+	SET @i = @i + 1
 END
 
-INSERT INTO medication
+INSERT INTO [emis].[dbo].[medication]
 SELECT * FROM #temp_medication
 DROP TABLE #temp_medication
