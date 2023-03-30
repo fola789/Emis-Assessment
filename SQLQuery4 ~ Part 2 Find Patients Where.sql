63444f9642c1a0282530eec14f3c103d5fa5d0b0
fola789 <49055102+fola789@users.noreply.github.com>
2023-03-30 04:47:20 +0100

    Create SQLQuery4 ~ Part 2 Find Patients Where.sql

---
+++ SQLQuery4 ~ Part 2 Find Patients Where.sql
@@ -0,0 +1,16 @@
+SELECT 
+	DISTINCT p.*
+FROM 
+	patient p 
+	INNER JOIN observation o ON p.["registration_guid"] = o.["registration_guid"]
+	INNER JOIN clinical_codes c ON (o.["emis_code_id"] = c.["code_id"] AND o.["snomed_concept_id"] = c.["snomed_concept_id"])
+	LEFT OUTER JOIN medication m ON p.["registration_guid"] = m.["registration_guid"]
+WHERE 
+	c.["refset_simple_id"] = 999012891000230104
+	AND c.["code_id"] IN (591221000033116, 717321000033118, 1215621000033114, 972021000033115, 1223821000033118)
+	AND c.["snomed_concept_id"] IN (129490002, 108606009, 702408004, 702801003, 704459002)
+	AND c.["refset_simple_id"] NOT IN (999004211000230104, 999011571000230107)
+	AND o.["dummy_patient_flag"] = 0
+	AND o.["confidential_flag"] = 0
+	AND m.["dummy_patient_flag"] = 0
+	AND m.["confidential_flag"] = 0;
