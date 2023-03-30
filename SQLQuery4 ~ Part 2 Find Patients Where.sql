d690cfe4acc95c93647e5c6908c2a330bd95eb78
fola789 <49055102+fola789@users.noreply.github.com>
2023-03-30 18:22:55 +0100

    Update SQLQuery4 ~ Part 2 Find Patients Where.sql

--- SQLQuery4 ~ Part 2 Find Patients Where.sql
+++ SQLQuery4 ~ Part 2 Find Patients Where.sql
@@ -1,10 +1,10 @@
 SELECT 
 	DISTINCT p.*
 FROM 
-	patient p 
-	INNER JOIN observation o ON p.["registration_guid"] = o.["registration_guid"]
-	INNER JOIN clinical_codes c ON (o.["emis_code_id"] = c.["code_id"] AND o.["snomed_concept_id"] = c.["snomed_concept_id"])
-	LEFT OUTER JOIN medication m ON p.["registration_guid"] = m.["registration_guid"]
+	[emis].[dbo].[patient] p 
+	INNER JOIN [emis].[dbo].[observation] o ON p.["registration_guid"] = o.["registration_guid"]
+	INNER JOIN [emis].[dbo].[clinical_codes] c ON (o.["emis_code_id"] = c.["code_id"] AND o.["snomed_concept_id"] = c.["snomed_concept_id"])
+	LEFT OUTER JOIN [emis].[dbo].[medication] m ON p.["registration_guid"] = m.["registration_guid"]
 WHERE 
 	c.["refset_simple_id"] = 999012891000230104
 	AND c.["code_id"] IN (591221000033116, 717321000033118, 1215621000033114, 972021000033115, 1223821000033118)
